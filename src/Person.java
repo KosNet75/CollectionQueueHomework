6a59c2df18afb240d77ab1baacba12c627423d01
KosNet75 <nemokonstantin@gmail.com>
2022-08-13 08:36:17 +0700

    test commit

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,13 @@
 public class Person {
+String firstName;
+  String secondName;
+  int tickets;
+
+
+  public Person(String firstName, String secondName, int tickets) {
+    this.firstName = firstName;
+    this.secondName = secondName;
+    this.tickets = tickets;
+  }
 
 }
