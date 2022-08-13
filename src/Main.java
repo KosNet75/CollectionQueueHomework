1d515cd4e140db515982e4bf905930658ad5f540
KosNet75 <nemokonstantin@gmail.com>
2022-08-13 08:36:23 +0700

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
+    List<Person> person = new LinkedList<>();
+
+    System.out.println(generateClients());
+
+  }
+
+
+  public static List<Person> generateClients() {
 
 
 
+
+//Person person = new Person("Gjdjdj","Ejddfjdj", 10);
 
 
   }
+
 }
+
