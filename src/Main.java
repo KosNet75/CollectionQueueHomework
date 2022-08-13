7cc2fe931987e0d5bf14488e2d2efdcf8b997ec2
KosNet75 <nemokonstantin@gmail.com>
2022-08-13 16:11:13 +0700

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,58 @@
-import java.util.LinkedList;
+
+
 import java.util.List;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+
 
 public class Main {
 
-  public static void main(String[] args) {
-    List<Person> person = new LinkedList<>();
 
-    System.out.println(generateClients());
+  public static List<Person> generateClients(List<Person> persons) {
 
-  }
-
-
-  public static List<Person> generateClients() {
-
-
-
-
-//Person person = new Person("Gjdjdj","Ejddfjdj", 10);
-
+    persons.add(new Person("Густов", "Константин", "1", 2));
+    persons.add(new Person("Федорова", "Ольга", "1", 5));
+    persons.add(new Person("Воронов", "Филипп", "1", 3));
+    persons.add(new Person("Алёна", "Загрекова", "1", 6));
+    persons.add(new Person("Капитан", "Немо", "1", 1));
+    persons.add(new Person("Ким", "5+", "1", 4));
+    return persons;
 
   }
+    public static void print(List<Person> list) {
+      for (int j = 0; j < list.size(); j++) {
+        System.out.println((j + 1) + "." +list.get(j));
+      }
+    }
+ // }
 
-}
+
+    public static void main(String[] args) {
+      List<Person> person = new LinkedList<>();
+
+
+     print(generateClients(person));
+    // System.out.println(generateClients(persons));
+
+
+      ArrayDeque<Person> client = new ArrayDeque<>();
+      // Первое добавление очереди.
+      for (Person value : person) {
+        client.addLast(value);
+      }
+
+      while (!client.isEmpty()) {
+        Person p = client.getFirst();
+
+        if (p.tickets > 0) {
+          p.tickets -= 1;
+          System.out.println(p.firstName + " " + p.secondName + " прокатился на атракционе у клиента осталось билетов "
+              + p.getCount());
+          client.addLast(p);
+        }
+        client.poll();
+      }
+    }
+  }
+
 
--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,32 @@
 public class Person {
 String firstName;
   String secondName;
+  String attraction;
   int tickets;
 
 
-  public Person(String firstName, String secondName, int tickets) {
+  public Person(String firstName, String secondName,String attraction, int tickets) {
     this.firstName = firstName;
     this.secondName = secondName;
     this.tickets = tickets;
+    this.attraction = attraction;
   }
 
+
+  @Override
+  public String toString() {
+    return "Person{" +
+        "name='" + firstName + '\'' +
+        ", lastName='" + secondName + '\'' +
+        ", count=" + tickets +
+        '}';
+  }
+
+  public void setCount(int count) {
+    this.tickets = count;
+  }
+
+  public int getCount() {
+    return tickets;
+  }
 }
